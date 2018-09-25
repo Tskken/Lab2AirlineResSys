669753128c812b06ff60ec4000b8ed3f2870fd0d
Finn Jensen <finn_jensen@outlook.com>
2018-09-25 02:26:51 -0400

    Customer and Itinerary Class
    Classes implented including getters, setters, and constructors.  Note:
    look at customer class for how to add itinerarys to a version of the
    class.

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private String mFName;
+    private String mLName;
+    private ArrayList<Itinerary> mItinerarys;
+
+    public Customer(String fName, String lName) {
+        this.mFName = fName;
+        this.mLName = lName;
+        this.mItinerarys = new ArrayList<Itinerary>();
+    }
+
+    public void addItinerarys(Itinerary newItinerary){
+        this.mItinerarys.add(newItinerary);
+    }
+
+    public String getmFName() {
+        return mFName;
+    }
+    public void setmFName(String mFName) {
+        this.mFName = mFName;
+    }
+    public String getmLName() {
+        return mLName;
+    }
+    public void setmLName(String mLName) {
+        this.mLName = mLName;
+    }
+    public ArrayList<Itinerary> getmItinerarys() {
+        return mItinerarys;
+    }
+    public void setmItinerarys(ArrayList<Itinerary> mItinerarys) {
+        this.mItinerarys = mItinerarys;
+    }
 }
--- src/Itinerary.java
+++ src/Itinerary.java
@@ -1,2 +1,30 @@
 public class Itinerary {
+    private Flight mFlight;
+    private Seat mSeat;
+    private Customer mCustomer;
+
+    public Itinerary(Flight flight, Seat seat, Customer customer) {
+        this.mFlight = flight;
+        this.mSeat = seat;
+        this.mCustomer = customer;
+    }
+
+    public Flight getmFlight() {
+        return mFlight;
+    }
+    public void setmFlight(Flight mFlight) {
+        this.mFlight = mFlight;
+    }
+    public Seat getmSeat() {
+        return mSeat;
+    }
+    public void setmSeat(Seat mSeat) {
+        this.mSeat = mSeat;
+    }
+    public Customer getmCustomer() {
+        return mCustomer;
+    }
+    public void setmCustomer(Customer mCustomer) {
+        this.mCustomer = mCustomer;
+    }
 }
